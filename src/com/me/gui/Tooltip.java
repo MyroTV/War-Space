5542b20517650b583c2136f00f06dffaa07cba20
MyroTV <michael.mcdougall92@gmail.com>
2015-10-12 21:32:20 +1100

    0.0.3.4.1
    Resync

---
+++ src/com/me/gui/Tooltip.java
@@ -0,0 +1,27 @@
+package com.me.gui;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.me.interfaces.GUIElement;
+
+public class Tooltip implements GUIElement {
+	
+	private String description;
+	private int height, width, posX, posY;
+	private int maxWidth, maxHeight;
+	private Sprite tooltipSprite;
+	private SpriteBatch batch;
+	
+	public Tooltip() {
+		
+	}
+	
+	public void init() {
+		
+	}
+
+	public void render() {
+		
+	}
+	
+}
